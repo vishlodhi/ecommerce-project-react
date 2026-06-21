3550411951c9d78813a1ddb069367c1a9dc95f9d
Vishal Lodhi <64070785+vishlodhi@users.noreply.github.com>
2026-06-21 19:35:30 +0530

    fix:correct bestseller filterning and product rendering issue

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -2,31 +2,44 @@
 import { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
+import ProductItem from './ProductItem';
 
 
 const BestSeller = () => {
 
     const { products } = useContext(ShopContext);
-    const [bestSeller, setBestSeller] = useState([]);
+    const [bestseller, setBestSeller] = useState([]);
 
-    useEffect(() => {
-        const bestProduct = products.filter((item) => (item.bestSeller));
-        setBestSeller(bestProduct.slice(0, 5));
-    }, [products])
+ useEffect(() => {
+    const bestProduct = products.filter(item => item.bestseller);
+    setBestSeller(bestProduct.slice(0, 5));    
+}, [products]);
 
 
     return (
         <div className='my-10'>
-            <div className='text-center py-8 text-3xl'>
+            <div className='text-center text-3xl py-8'>
                 <Title text1={'Best'} text2={'Seller'} />
                 <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600' >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, esse?
-            </p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, esse?
+                </p>
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+                {
+                    bestseller.map((item, index) => (
+                        <ProductItem
+                            key={index}
+                            id={item._id}
+                            name={item.name}
+                            image={item.image}
+                            price={item.price}
+                        />
+                    ))
 
-             </div>
-
+                }
+            </div>
         </div>
-             )
+    )
 }
 
 export default BestSeller
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,19 +1,19 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import { Link } from 'react-router-dom'
+    import { useContext } from 'react'
+    import { ShopContext } from '../context/ShopContext'
+    import { Link } from 'react-router-dom'
 
-const ProductItem = ({ id, image, name, price }) => {
+    const ProductItem = ({ id, image, name, price }) => {
 
-    const { currency } = useContext(ShopContext);
-    return (
-        <Link to={`/product/${id}`} className='block'>
-            <div>
-                <img src={image[0]} alt="name" className='w-full' />
-                <p>{name}</p>
-                <p>{currency}{price}</p>
-            </div>
-        </Link>
-    )   
-}
+        const { currency } = useContext(ShopContext);
+        return (
+            <Link to={`/product/${id}`} className='block'>
+                <div>
+                    <img src={image[0]} alt="name" className='w-full' />
+                    <p>{name}</p>
+                    <p>{currency}{price}</p>
+                </div>
+            </Link>
+        )   
+    }
 
-export default ProductItem
+    export default ProductItem

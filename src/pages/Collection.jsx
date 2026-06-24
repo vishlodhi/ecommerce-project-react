2823a1d9663d24cc06d31845b0a0fa3f3fedc41d
Vishal Lodhi <64070785+vishlodhi@users.noreply.github.com>
2026-06-24 07:02:35 +0530

    Update the Collections

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,10 +1,17 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import Title from '../components/Title'
+import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  useEffect(()=>{
+    setFilterProducts(products);
+  },[])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -43,6 +50,34 @@ const Collection = () => {
           </div>
         </div>
       </div>
+      {/* Riigt Side */}
+      <div className='flex-1'>
+          <div className='flex justify-between mb-4 text:base sm:text-2xl'>
+            <Title text1={'All'} text2={'Collections'} />    
+            {/* Product Sort */}
+            <select name="" className='border border-gray-400 px-2 text:sm'>
+              <option value="Relevant">Relevant</option>
+              <option value="Low to High">Low to High</option>
+              <option value="High to Low">High to Low</option>
+            </select>
+          </div>
+          {/* Map Product */}
+          <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'> 
+            {
+              filterProducts.map((item,index)=>(
+                 < ProductItem 
+                 key={index}
+                 id={item._id}
+                 image={item.image}
+                 name={item.name}
+                 price={item.price}
+
+                 /> 
+              ))
+            }
+          </div>
+      </div>
+
     </div>
   )
 }

c658063c09ab7e3f1cdc93d58dc770b186ae5a06
Vishal Lodhi <64070785+vishlodhi@users.noreply.github.com>
2026-06-30 23:57:03 +0530

    Update to Searchbad adn collection

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import { useLocation } from 'react-router-dom';
 
 const SearchBar = () => {
 
     const {search, setSearch, showSearch, setShowSearch}  = useContext(ShopContext);
+    const location = useLocation();
+    const [visible,setVisible] = useState(false);
 
-  return showSearch ? (
+ useEffect(()=>{
+  if(location.pathname.includes('collection')){
+    setVisible(true);
+  }
+  else {
+    setVisible(false)
+  }
+ },[location])
+  return showSearch && visible ? (
     <div className='border-t border-b bg-gray-50 text-center' >
       <div className='inline-flex items-center justify-center border border-gray-400 rounded-full px-5 py-3 mx-5 my-3 w-3/4 sm:w-1/3'>
       <input value={search} onChange={(e)=>setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search'  />

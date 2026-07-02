6dfabb44e91ebbcfe7c898f0bc1b34a7723be836
Vishal Lodhi <64070785+vishlodhi@users.noreply.github.com>
2026-07-02 08:09:47 +0530

    update the product.jsx page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { products } from '../assets/assets'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('');
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        console.log(item);
+        return null;
+      }
+
+    })
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, [products, productId])
+
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product Data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        {/* PRoduct Image */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item,index) => (
+                <img onClick={()=>setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+              <img  className='w-full h-auto' src={image} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : <div className='opacity-0'>
+
+  </div>
 }
 
 export default Product
